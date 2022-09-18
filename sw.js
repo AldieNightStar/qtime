13f7bd020b61cda547e6bd4d4715eb798545342e
AldieNightStar <Aldie.NightStar@gmail.com>
2022-09-18 16:20:58 +0200

    sw

--- sw.js
+++ sw.js
@@ -16,3 +16,36 @@ self.addEventListener('install', function(event) {
             })
     );
 });
+
+// When trying to get something from internet
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    // The requested file exists in the cache so we return it from the cache.
+                    return response;
+                }
+
+                // The requested file is not present in cache so we send it forward to the internet
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = []; // add cache names which you do not want to delete
+    cacheWhitelist.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
